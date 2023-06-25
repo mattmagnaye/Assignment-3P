b443c70e2bc40e3f6a19ed3ca23209066ac53a4f
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-25 15:12:20 -0700

    code very confusing

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 * Author: Matt Aaron Magnaye
 * Instructor: Professor David Harden
 * Date: 06/25/2023
-* Description: The program plays a number guessing game where the user thinks of a number within a given range,
-*              and the program makes guesses based on user feedback to determine the correct number.   
+* Description: It is a number guessing game with a Human User.
+*
 */
 
 #include <iostream>
 using namespace std;
 
-void getUserResponseToGuess(int guess, char& result)
+const int LOWER_LIMIT = 1;
+const int UPPER_LIMIT = 100;
 
+void getUserResponseToGuess(int guess, char& result)
+    cout << "My guess is " << 
+    
 
 int getMidpoint(int low, int high)
 
@@ -25,9 +29,23 @@ int main() {
         cout << "Great! Do you want to play again (y/n)? ";
         cin >> response;
     }
-    
+    return 0;
 }
 
 void playOneGame () {
-    int 
+    int lowerlimit = LOWER_LIMIT;
+    int higherlimit = UPPER_LIMIT;
+    char response;
+
+    cout << "Think of a number between " << LOWER_LIMIT << " and " << UPPER_LIMIT << "." << endl;
+
+    while (true) {
+        int guess = getMidpoint(lowerlimit, higherlimit);
+
+        getUserResponseToGuess(guess, response)
+
+        if (response == 'l')
+    }
+
+
 }
