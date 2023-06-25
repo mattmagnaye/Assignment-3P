2574b966dcb078dcd7d6875e23d6c86330e0214e
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-25 14:58:04 -0700

    added required code for instruction

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+* Program: Number guessing game - Project 7.4 (Assignment 3P: Functions)
+* Author: Matt Aaron Magnaye
+* Instructor: Professor David Harden
+* Date: 06/25/2023
+* Description: The program plays a number guessing game where the user thinks of a number within a given range,
+*              and the program makes guesses based on user feedback to determine the correct number.   
+*/
+
+#include <iostream>
+using namespace std;
+
+void getUserResponseToGuess(int guess, char& result)
+
+
+int getMidpoint(int low, int high)
+
+int main() {
+    char response;
+    
+    cout << "Ready to play (y/n)? ";
+    cin >> response;
+    while (response == 'y') {
+        playOneGame();
+        cout << "Great! Do you want to play again (y/n)? ";
+        cin >> response;
+    }
+    
+}
+
+void playOneGame () {
+    int 
+}
